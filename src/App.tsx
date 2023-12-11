1bd3abe50d08d57a92fb371e791b0c153c16aad6
Josh Tucker <joshuatucker648@gmail.com>
2023-12-11 22:15:19 +0000

    Added workout card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import './App.css'
 import TopBar from './components/TopBar'
 import BottomBar from './components/BottomBar'
+import WorkoutCard from './components/WorkoutCard'
 
 function App() {
   return (
     <>
       <TopBar/>
       <BottomBar/>
+      <div className='scrollbox'>
+        <div className='header'>Next Workout</div>
+        <WorkoutCard/>
+        <div className='header'>History</div>
+        <WorkoutCard/>
+        <WorkoutCard/>
+        <WorkoutCard/>
+        <WorkoutCard/>
+        <WorkoutCard/>
+        <WorkoutCard/>
+        <WorkoutCard/>
+        <WorkoutCard/>
+        <WorkoutCard/>
+        <WorkoutCard/>
+
+      </div>
     </>
   )
 }
---
+++ src/components/WorkoutCard.tsx
@@ -0,0 +1,11 @@
+import './WorkoutCard.css'
+
+function WorkoutCard() {
+    return (
+      <div className='card'>
+        
+      </div>
+    )
+  }
+  
+export default WorkoutCard
