86c021069984941c93c544e6d358f718b4dc7747
Josh Tucker <joshuatucker648@gmail.com>
2023-12-11 22:21:21 +0000

    Made top bar fixed

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,7 +3,9 @@ import './TopBar.css'
 function TopBar() {
     return (
       <div className='topBar'>
-        <div className='title'>Gym Tracker</div>
+        <div className='titleContainer'>
+          <div className='title'>Gym Tracker</div>
+        </div>
       </div>
     )
   }
