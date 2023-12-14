f391a21d5d0a38195c43f14ab0599a9e96773a00
Josh Tucker <joshuatucker648@gmail.com>
2023-12-14 00:43:23 +0000

    Added table to exercise card

--- src/components/WorkoutScreen/ExerciseCard.tsx
+++ src/components/WorkoutScreen/ExerciseCard.tsx
@@ -4,6 +4,32 @@ export default function ExerciseCard() {
   return (
     <div className='card'>
       <div>Squat</div>
+      <table className='table'>
+        <tr>
+          <th>Set</th>
+          <th>kg</th>
+          <th>Reps</th>
+          <th>done</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td><input type="number"/></td>
+          <td><input type="number"/></td>
+          <td><input type="checkbox"/></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td><input type="number"/></td>
+          <td><input type="number"/></td>
+          <td><input type="checkbox"/></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td><input type="number"/></td>
+          <td><input type="number"/></td>
+          <td><input type="checkbox"/></td>
+        </tr>
+      </table>
     </div>
   )
 }
