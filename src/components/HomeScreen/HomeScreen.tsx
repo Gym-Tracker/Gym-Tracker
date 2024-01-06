172ef3d26a0ae00af69f53fe91db7b38c4765871
jtucker2 <joshuatucker648@gmail.com>
2024-01-06 17:47:38 +0000

    Can now fetch workouts from server and display in UI

--- src/components/HomeScreen/HomeScreen.tsx
+++ src/components/HomeScreen/HomeScreen.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Workout } from '../../types'
 import WorkoutCard from "./WorkoutCard/WorkoutCard"
 
@@ -48,23 +49,28 @@ const workout : Workout = {
   ]
 }
 
-
 export default function HomeScreen() {
+  const [workouts, setWorkouts] = useState<Workout[]>([workout, workout, workout])
+
+  async function getWorkouts() {
+    const response = await fetch("http://127.0.0.1:8080/workout");
+    const workoutsResponse: Workout[] = await response.json();
+
+    // Date of workout comes as a string a needs to be a Date object
+    for (let i = 0; i < workoutsResponse.length; i++) {
+      workoutsResponse[i].date = new Date(workoutsResponse[i].date);
+    }
+
+    setWorkouts(workoutsResponse);
+  }
+  
   return (
     <>
       <div className='header'>Next Workout</div>
       <WorkoutCard workout={workout}/>
       <div className='header'>History</div>
-      <WorkoutCard workout={workout}/>
-      <WorkoutCard workout={workout}/>
-      <WorkoutCard workout={workout}/>
-      <WorkoutCard workout={workout}/>
-      <WorkoutCard workout={workout}/>
-      <WorkoutCard workout={workout}/>
-      <WorkoutCard workout={workout}/>
-      <WorkoutCard workout={workout}/>
-      <WorkoutCard workout={workout}/>
-      <WorkoutCard workout={workout}/>
+      <div onClick={getWorkouts}>refresh</div>
+      { workouts.map((workout) => <WorkoutCard workout={workout}/>) }
     </>
   )
 }
