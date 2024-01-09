ebfa66512e3eeb36b012030ac844c5624396af01
jtucker2 <joshuatucker648@gmail.com>
2024-01-09 17:33:06 +0000

    Added test for converting active workout to workout ready to send to the server

--- src/components/WorkoutScreen/WorkoutScreen.test.ts
+++ src/components/WorkoutScreen/WorkoutScreen.test.ts
@@ -1,5 +1,5 @@
-import { ActiveWorkout, Routine } from "../../types";
-import { routineToActiveWorkout } from "./WorkoutScreen";
+import { ActiveWorkout, Routine, Workout } from "../../types";
+import { activeWorkoutToWorkout, routineToActiveWorkout } from "./WorkoutScreen";
 
 const routine : Routine = {
   exercises: [
@@ -110,4 +110,87 @@ const activeWorkout : ActiveWorkout = {
 
 test('convert routine to active workout', () => {
   expect(routineToActiveWorkout(routine)).toStrictEqual(activeWorkout);
+});
+
+const exampleActiveWorkout : ActiveWorkout = {
+  exercises: [
+    {
+      id: 2,
+      sets: [
+        {
+          type: 0,
+          weight: 30,
+          reps: 5,
+          done: true
+        },
+        {
+          type: 0,
+          weight: 35,
+          reps: 5,
+          done: false
+        },
+        {
+          type: 0,
+          weight: 37.5,
+          reps: 8,
+          done: true
+        },
+        {
+          type: 0,
+          weight: 30,
+          reps: 5,
+          done: false
+        }
+      ]
+    },
+    {
+      id: 1,
+      sets: [
+        {
+          type: 0,
+          weight: 25,
+          reps: 5,
+          done: false
+        },
+        {
+          type: 0,
+          weight: 27.5,
+          reps: 5,
+          done: false
+        },
+        {
+          type: 0,
+          weight: 32.5,
+          reps: 8,
+          done: false
+        }
+      ]
+    }
+  ]
+}
+
+const convertedWorkout : Workout = {
+  date: new Date(),
+  duration: 3600,
+  exercises: [
+    {
+      id: 2,
+      sets: [
+        {
+          type: 0,
+          weight: 30,
+          reps: 5,
+        },
+        {
+          type: 0,
+          weight: 37.5,
+          reps: 8,
+        }
+      ]
+    }
+  ]
+}
+
+test('convert active workout to workout', () => {
+  expect(activeWorkoutToWorkout(exampleActiveWorkout).exercises).toStrictEqual(convertedWorkout.exercises);
 });
