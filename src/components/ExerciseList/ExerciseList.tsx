da8840aa1c003e32f350692934551a91b2358ada
jtucker2 <joshuatucker648@gmail.com>
2024-01-11 10:40:32 +0000

    Added search for exercise list

--- src/components/ExerciseList/ExerciseList.tsx
+++ src/components/ExerciseList/ExerciseList.tsx
@@ -1,26 +1,39 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { ExerciseDetails } from "../../types";
 import styles from './ExerciseList.module.css'
 import ExerciseInfoCard from "./ExerciseInfoCard";
 
 export default function ExerciseList() {
-  const [exerciseList, setExercistList] = useState<ExerciseDetails[]>([]);
+  const [search, setSearch] = useState<string>("");
+  const [filteredExerciseList, setFilteredExercistList] = useState<ExerciseDetails[]>([]);
+  const exerciseList = useRef<ExerciseDetails[]>([]);
   
   async function printJSON() {
     const response = await fetch("src/exercises.json");
-    const json = await response.json();
-    setExercistList(json);
+    exerciseList.current = await response.json();
+    setFilteredExercistList(exerciseList.current);
   }
-
+  
   useEffect(() => {
     printJSON();
   }, [])
 
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const searchValue = e.target.value;
+    setSearch(searchValue);
+
+    const filteredItems = exerciseList.current.filter((exercise) => 
+      exercise.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+
+    setFilteredExercistList(filteredItems);
+  }
 
   return(
     <>
       <div className={styles.container}>
-        { exerciseList.map((exerciseDetails) => 
+        <input className={styles.search} value={search} onChange={(e) => handleInputChange(e)}></input>
+        { filteredExerciseList.map((exerciseDetails) => 
             <>
               <ExerciseInfoCard exerciseDetails={exerciseDetails}/>
               <div className={styles.divider}></div>
