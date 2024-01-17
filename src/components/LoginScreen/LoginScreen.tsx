cb284005885a0d37c7b808293a9d997f4ef24ec0
jtucker2 <joshuatucker648@gmail.com>
2024-01-17 21:48:35 +0000

    Added function for logging in

--- src/components/LoginScreen/LoginScreen.tsx
+++ src/components/LoginScreen/LoginScreen.tsx
@@ -9,6 +9,21 @@ export default function LoginScreen({ setLoggedIn }: props) {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
+  async function logIn() {
+    const response = await fetch(import.meta.env.VITE_SERVER_URL + "/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    });
+
+    const loggenIn: boolean = await response.json();
+
+    setLoggedIn(loggenIn);
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
@@ -27,7 +42,7 @@ export default function LoginScreen({ setLoggedIn }: props) {
             value={password}
             onChange={e => setPassword(e.target.value)}
           />
-          <div className={styles.button} onClick={() => setLoggedIn(true)}>Log In</div>
+          <div className={styles.button} onClick={logIn}>Log In</div>
         </form>
         <div>Don't have an account? Create One</div>
       </div>
