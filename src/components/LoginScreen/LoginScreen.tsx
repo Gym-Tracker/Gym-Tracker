307d28fbbf20315967998cf91ed13dac4e015b1b
jtucker2 <joshuatucker648@gmail.com>
2024-01-19 00:17:28 +0000

    Added register page

--- src/components/LoginScreen/LoginScreen.tsx
+++ src/components/LoginScreen/LoginScreen.tsx
@@ -8,6 +8,7 @@ interface props {
 export default function LoginScreen({ setLoggedIn }: props) {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [loggingIn, setLoggingIn] = useState<boolean>(false);
 
   async function logIn() {
     const response = await fetch(import.meta.env.VITE_SERVER_URL + "/login", {
@@ -29,23 +30,55 @@ export default function LoginScreen({ setLoggedIn }: props) {
     <div className={styles.container}>
       <div className={styles.formContainer}>
         <div className={styles.title}>Gym Tracker</div>
-        <form className={styles.form}>
-          <input
-            className={styles.formInput}
-            placeholder="Email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-          <input
-            className={styles.formInput}
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-          <div className={styles.button} onClick={logIn}>Log In</div>
-        </form>
-        <div>Don't have an account? Create One</div>
+        {
+          loggingIn ?
+            <>
+              <form className={styles.form}>
+                <input
+                  className={styles.formInput}
+                  placeholder="Email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                />
+                <input
+                  className={styles.formInput}
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+                <div className={styles.button} onClick={logIn}>Log In</div>
+              </form>
+              <div>Don't have an account? Create One</div>
+            </>
+          :
+            <>
+              <form className={styles.form}>
+                <input
+                  className={styles.formInput}
+                  placeholder="Email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                />
+                <input
+                  className={styles.formInput}
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+                <input
+                  className={styles.formInput}
+                  type="password"
+                  placeholder="Confirm Password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+                <div className={styles.button} onClick={logIn}>Register</div>
+              </form>
+              <div>Have an account? Log In</div>
+            </>
+        }
       </div>
     </div>
   )
