c79d0fb3ded562d8e27c694cd333dbe698718e7f
jtucker2 <joshuatucker648@gmail.com>
2024-01-30 20:19:30 +0000

    Made routine cards display comma separated list of exercise names

--- src/components/HomeScreen/RoutineCard/RoutineCard.tsx
+++ src/components/HomeScreen/RoutineCard/RoutineCard.tsx
@@ -1,18 +1,17 @@
 import { getExerciseName } from "../../../Exercises";
-import { Routine } from "../../../types";
+import { Exercise, Routine } from "../../../types";
 import styles from './RoutineCard.module.css'
+
+function exerciseNamesString(exercises: Exercise[]) {
+  return exercises.map(exercise => getExerciseName(exercise.id)).join(", ");
+}
 
 export default function RoutineCard({ routine }: { routine: Routine }) {
   return (
     <div className={styles.card}>
       <div><b>{routine.name}</b></div>
-      {
-        routine.exercises.map(exercise => 
-          <div>
-            {getExerciseName(exercise.id)}
-          </div>
-        )
-      }
+      <div>{exerciseNamesString(routine.exercises)}</div>
+      <div className={styles.button}>Start Routine</div>
     </div>
   )
 }
